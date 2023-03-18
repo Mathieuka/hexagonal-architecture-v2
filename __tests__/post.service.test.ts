7ed494e8bd3c2310868016ee93609e0eb501e37f
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2023-03-18 21:04:46 +0100

    test(post): pass http client

--- __tests__/post.service.test.ts
+++ __tests__/post.service.test.ts
@@ -1,37 +1,44 @@
 import { postService } from "@domain/services/Post.services";
 import { postRepository } from "@infrastructure/repositories/postRepository";
+import { httpAxios } from "@infrastructure/instances/httpAxios";
 
 describe("Post service", () => {
   test("Get posts", async () => {
-    const posts = await postService(postRepository()).getPosts();
+    const posts = await postService(postRepository(httpAxios)).getPosts();
     expect(posts.length).toEqual(6);
   });
 
   test("Add post", async () => {
-    const posts = await postService(postRepository()).getPosts();
+    const posts = await postService(postRepository(httpAxios)).getPosts();
     const newPost = {
       userId: 3,
       id: posts.length,
       title: "Craft",
       body: "Hexagonal architecture",
     };
-    const newPosts = postService(postRepository()).createPost(newPost, posts);
+    const newPosts = postService(postRepository(httpAxios)).createPost(
+      newPost,
+      posts
+    );
 
     expect(newPosts[0]).toEqual(newPost);
     expect(newPosts.length).toEqual(7);
   });
 
   test("Delete post", async () => {
-    const posts = await postService(postRepository()).getPosts();
+    const posts = await postService(postRepository(httpAxios)).getPosts();
     const postId = 1;
 
     expect(posts.find((post) => post.id === postId)).toBeTruthy();
-    const newPosts = postService(postRepository()).deletePost(postId, posts);
+    const newPosts = postService(postRepository(httpAxios)).deletePost(
+      postId,
+      posts
+    );
     expect(newPosts.find((post) => post.id === postId)).toEqual(undefined);
   });
 
   test("Update post", async () => {
-    const posts = await postService(postRepository()).getPosts();
+    const posts = await postService(postRepository(httpAxios)).getPosts();
     const postId = 1;
     const post = posts.find((post) => post.id === postId);
 
@@ -39,7 +46,7 @@ describe("Post service", () => {
       "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
     );
 
-    const newPosts = postService(postRepository()).updatePost(
+    const newPosts = postService(postRepository(httpAxios)).updatePost(
       postId,
       { title: "hexagonal" },
       posts
--- infrastructure/repositories/postRepository.ts
+++ infrastructure/repositories/postRepository.ts
@@ -2,28 +2,27 @@ import { Http } from "@domain/repositories/Http";
 import { PostRepository } from "@domain/repositories/Post";
 import { PostDTO } from "@infrastructure/http/dto/postDTO";
 
-export const postRepository = ((client: Http): PostRepository  => ({
-    getPosts: async () => {
-      return await client.get<PostDTO>(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-    },
-    createPost: (newPost, posts) => {
-      const post = { userId: 1, id: posts.length + 1, ...newPost };
-      return [post, ...posts];
-    },
-    deletePost: (id, posts) => posts.filter((post) => post.id !== id),
-    updatePost: (id, params, posts) => {
-      const index = posts.findIndex((post) => (post.id = id));
+export const postRepository = (client: Http): PostRepository => ({
+  getPosts: async () => {
+    return await client.get<PostDTO>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+  },
+  createPost: (newPost, posts) => {
+    const post = { userId: 1, id: posts.length + 1, ...newPost };
+    return [post, ...posts];
+  },
+  deletePost: (id, posts) => posts.filter((post) => post.id !== id),
+  updatePost: (id, params, posts) => {
+    const index = posts.findIndex((post) => post.id === id);
 
-      if (index >= 0) {
-        posts[index] = {
-          ...posts[index],
-          ...params,
-        };
-      }
+    if (index >= 0) {
+      posts[index] = {
+        ...posts[index],
+        ...params,
+      };
+    }
 
-      return posts;
-    },
-  }));
-
+    return posts;
+  },
+});
