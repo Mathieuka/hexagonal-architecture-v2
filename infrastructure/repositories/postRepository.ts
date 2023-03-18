aceb7da8bbbff62584170525aa95adbcb60ab758
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2023-03-18 20:12:41 +0100

    chore(postRepository): remove iife and move instance modules in its own scope

--- infrastructure/repositories/postRepository.ts
+++ infrastructure/repositories/postRepository.ts
@@ -1,10 +1,8 @@
 import { Http } from "@domain/repositories/Http";
 import { PostRepository } from "@domain/repositories/Post";
 import { PostDTO } from "@infrastructure/http/dto/postDTO";
-import { httpAxios } from "@infrastructure/instances/httpAxios";
 
-export const postRepository = () =>
-  ((client: Http): PostRepository => ({
+export const postRepository = ((client: Http): PostRepository  => ({
     getPosts: async () => {
       return await client.get<PostDTO>(
         "https://jsonplaceholder.typicode.com/posts"
@@ -27,4 +25,5 @@ export const postRepository = () =>
 
       return posts;
     },
-  }))(httpAxios);
+  }));
+
