bf1e05db2047bd0ca85b1d0dc0e76250c91811c1
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2023-03-19 23:45:55 +0100

    test(jest): setup config

---
+++ infrastructure/view/my-post/jest.config.js
@@ -0,0 +1,27 @@
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "^@/hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@/domain/services/(.*)$": "<rootDir>/../../../domain/services/$1",
+    "^@/infrastructure/repositories/(.*)$":
+      "<rootDir>/../../../infrastructure/repositories/$1",
+    "^@/infrastructure/instances/(.*)$":
+      "<rootDir>/../../../infrastructure/instances/$1",
+  },
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
